67431269655f32cf9f1e73b48da5f58dc8bc1666
Bruno Abdon <bru.n.oa.b.don@gmail.com>
2015-10-14 13:02:40 -0300

    limpando codigo

--- src/main/java/br/nom/abdon/modelo/EntidadeBaseInt.java
+++ src/main/java/br/nom/abdon/modelo/EntidadeBaseInt.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.nom.abdon.modelo;
 
-import br.nom.abdon.gastoso.Conta;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -47,7 +41,4 @@ public class EntidadeBaseInt implements Entidade<Integer>{
 
         return entidade;
     }
-
-    
-
 }
