d32a209917d58a65d6493559cede0237436270c6
brunoabdon <bru.n.oa.b.don@gmail.com>
2015-10-29 09:05:45 -0200

    load: removendo erro de arredondamentos. inserindo fato final de correcao de erros

--- src/main/java/br/nom/abdon/gastoso/load/Main.java
+++ src/main/java/br/nom/abdon/gastoso/load/Main.java
@@ -7,10 +7,14 @@ import br.nom.abdon.heroku.HerokuUtils;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
@@ -65,14 +69,21 @@ public class Main {
             final Workbook wb = new XSSFWorkbook(arquivoXLS);
             final Sheet sheet = wb.getSheet("Contas");
             final Row linhaContas = sheet.getRow(0);
+            final Row linhaSaldos = sheet.getRow(2);
             final int numCells = linhaContas.getLastCellNum() - 1;
+            final int numContas = numCells-2;
             
-            final Conta[] contas = new Conta[numCells-2];
+            final Conta[] contas = new Conta[numContas];
+            final int saldos[] = new int[numContas];
+            
+            final FormulaEvaluator evaluator = 
+                wb.getCreationHelper().createFormulaEvaluator();
             
             em.getTransaction().begin();
             //contas
-            for (short i = 2; i < numCells; i++) {
-                final Cell cellConta = linhaContas.getCell(i);
+            for (short i = 0; i < numContas; i++) {
+                final int cellIdx = i+2;
+                final Cell cellConta = linhaContas.getCell(cellIdx);
                 final String nomeDaConta = 
                     cellConta
                     .getStringCellValue()
@@ -80,12 +91,24 @@ public class Main {
                 
                 final Conta conta = new Conta(nomeDaConta);
                 em.persist(conta);
-                contas[i-2] = conta;
+                
+                final Cell cellSaldo = linhaSaldos.getCell(cellIdx);
+                
+                evaluator.evaluate(cellSaldo);
+                
+                final String strValor = 
+                    cellSaldo
+                    .getStringCellValue()
+                    .replace("R$ ", "")
+                    .replace(",", "")
+                    .replace(".", "");
+                
+                final int saldo = Integer.parseInt(strValor);
+
+                contas[i] = conta;
+                saldos[i] = saldo;
             }
            
-            final FormulaEvaluator evaluator = 
-                wb.getCreationHelper().createFormulaEvaluator();
-            
             LocalDate date = null;
             short rowNum = 3;
 
@@ -112,8 +135,9 @@ public class Main {
                 
                 em.persist(fato);
                 
-                for (short i = 2; i < numCells; i++) {
-                    final Cell celulaLancamento = linhaFato.getCell(i);
+                for (short i = 0; i < numContas; i++) {
+                    final int cellIdx = i+2;
+                    final Cell celulaLancamento = linhaFato.getCell(cellIdx);
                     final int cellType = celulaLancamento.getCellType();
 
                     double cellValue;
@@ -122,7 +146,7 @@ public class Main {
                         case Cell.CELL_TYPE_FORMULA:
                             final CellValue formulaValue = 
                                 evaluator.evaluate(celulaLancamento);
-
+                            
                             cellValue = formulaValue.getNumberValue();
                             break;
                         case Cell.CELL_TYPE_NUMERIC:
@@ -136,17 +160,46 @@ public class Main {
                                 String.valueOf(celulaLancamento.getCellType()));
                     }
 
-                    int valor = (int)Math.floor(cellValue*100d);
-                    Conta conta = contas[i-2];
-                    Lancamento lancamento = new Lancamento(fato,conta,valor);
+                    final int valor = 
+                        BigDecimal
+                            .valueOf(cellValue)
+                            .movePointRight(2)
+                            .setScale(0, RoundingMode.HALF_UP)
+                            .intValueExact();
+                    
+                    final Conta conta = contas[i];
+                    final Lancamento lancamento = new Lancamento(fato,conta,valor);
 
-                    System.out.printf("\t\t%s R$ %f\n",
-                        lancamento.getConta().getNome(),
-                        BigDecimal.valueOf(lancamento.getValor(), 2).floatValue());
+                    System.out.printf("\t\t%s R$ %.2f\n",
+                        conta.getNome(),
+                        BigDecimal.valueOf(valor, 2).doubleValue());
                     
                     em.persist(lancamento);
+                    
+                    saldos[i]-=valor;
                 }
                 rowNum++;
+            }
+
+            
+            //fechando
+            final Fato fatoFechamento = new
+                Fato(LocalDate.now(),"Erro importação");
+
+            final Stack<Lancamento> lancamentosErros = new Stack<>();
+            
+            for (short i = 0;  i < numContas; i++) {
+                final int saldo = saldos[i];
+                if(saldo != 0){
+                    Lancamento lancamento = 
+                        new Lancamento(fatoFechamento, contas[i], saldo);
+                    lancamentosErros.push(lancamento);
+                    
+                }
+            }
+            if(!lancamentosErros.empty()){
+                em.persist(fatoFechamento);
+                lancamentosErros.stream().forEach(em::persist);
             }
             
             em.getTransaction().commit();
