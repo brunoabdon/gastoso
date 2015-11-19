a9edfb0f519db48a28a4669bfd3a72a10e558074
brunoabdon <bru.n.oa.b.don@gmail.com>
2015-11-19 10:51:59 -0200

    criando servico MaisRs com consultas detalhadas. Voltando consulta de fatos pra normal

--- src/main/java/br/nom/abdon/gastoso/rest/Fatos.java
+++ src/main/java/br/nom/abdon/gastoso/rest/Fatos.java
@@ -3,13 +3,10 @@ package br.nom.abdon.gastoso.rest;
 import br.nom.abdon.gastoso.Fato;
 import br.nom.abdon.gastoso.dal.FatosDao;
 import br.nom.abdon.gastoso.dal.LancamentosDao;
-import br.nom.abdon.gastoso.rest.model.FatoDetalhe;
 import br.nom.abdon.rest.AbstractRestCrud;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,7 +14,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
@@ -54,9 +50,7 @@ public class Fatos extends AbstractRestCrud<Fato,Integer>{
         @QueryParam("dataMax") LocalDate dataMaxima){
 
         final List<Fato> fatos;
-        Response.ResponseBuilder builder;
-        
-        
+
         if((mes == null && dataMaxima == null)
             || (mes != null && (dataMaxima != null || dataMinima != null))){
             throw new WebApplicationException(Response.Status.BAD_REQUEST);
@@ -70,35 +64,13 @@ public class Fatos extends AbstractRestCrud<Fato,Integer>{
         }
             
         final EntityManager entityManager = emf.createEntityManager();
-
-        final Function<Fato, FatoDetalhe> detalhaFato = 
-            fato -> new FatoDetalhe(
-                            fato, 
-                            lancamentosDao.listar(entityManager, fato));
         
         try {
             fatos = dao.listar(entityManager, dataMinima, dataMaxima);
-            
-            final List<FatoDetalhe> fatosDetalhados = new ArrayList<>(fatos.size());
-            
-            fatos.stream().map(detalhaFato).forEach(fatosDetalhados::add);
-            
-            
-            final br.nom.abdon.gastoso.rest.model.Fatos fs = new br.nom.abdon.gastoso.rest.model.Fatos(dataMinima,dataMaxima,fatosDetalhados);
-
-            final EntityTag tag = new EntityTag(Integer.toString(fs.hashCode()));
-            builder = request.evaluatePreconditions(tag);
-            if(builder==null){
-		//preconditions are not met and the cache is invalid
-		//need to send new value with reponse code 200 (OK)
-		builder = Response.ok(fs);
-		builder.tag(tag);
-            }
-            
         } finally {
             entityManager.close();
         }
         
-        return builder.build();
+        return buildResponse(request, fatos);
     }
 }
---
+++ src/main/java/br/nom/abdon/gastoso/rest/MaisRs.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2015 Bruno Abdon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.nom.abdon.gastoso.rest;
+
+import br.nom.abdon.gastoso.Fato;
+import br.nom.abdon.gastoso.dal.FatosDao;
+import br.nom.abdon.gastoso.dal.LancamentosDao;
+import br.nom.abdon.gastoso.rest.model.FatoDetalhe;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Bruno Abdon
+ */
+@Produces(MediaType.APPLICATION_JSON)
+@Path("")
+public class MaisRs {
+
+    @PersistenceUnit(unitName = "gastoso_peruni")
+    protected EntityManagerFactory emf;
+
+    private final FatosDao dao;
+    private final LancamentosDao lancamentosDao;
+
+    public MaisRs() {
+        this.dao = new FatosDao();
+        this.lancamentosDao = new LancamentosDao();
+    }
+    
+    @GET
+    @Path("fatosDetalhados")
+    public Response lista(
+        final @Context Request request,
+        final @QueryParam("mes") YearMonth mes,
+        @QueryParam("dataMin") LocalDate dataMinima,
+        @QueryParam("dataMax") LocalDate dataMaxima){
+
+        final List<Fato> fatos;
+        Response.ResponseBuilder builder;
+        
+        if((mes == null && dataMaxima == null)
+            || (mes != null && (dataMaxima != null || dataMinima != null))){
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        
+        if(mes != null){
+            dataMinima = mes.atDay(1);
+            dataMaxima  = mes.atEndOfMonth();
+        } else if(dataMinima == null) {
+            dataMinima = dataMaxima.minusMonths(1);
+        }
+            
+        final EntityManager entityManager = emf.createEntityManager();
+
+        final Function<Fato, FatoDetalhe> detalhaFato = 
+            fato -> new FatoDetalhe(
+                            fato, 
+                            lancamentosDao.listar(entityManager, fato));
+        
+        try {
+            fatos = dao.listar(entityManager, dataMinima, dataMaxima);
+            
+            final List<FatoDetalhe> fatosDetalhados = new ArrayList<>(fatos.size());
+            
+            fatos.stream().map(detalhaFato).forEach(fatosDetalhados::add);
+            
+            final br.nom.abdon.gastoso.rest.model.Fatos fs = new br.nom.abdon.gastoso.rest.model.Fatos(dataMinima,dataMaxima,fatosDetalhados);
+
+            final EntityTag tag = new EntityTag(Integer.toString(fs.hashCode()));
+            builder = request.evaluatePreconditions(tag);
+            if(builder==null){
+		//preconditions are not met and the cache is invalid
+		//need to send new value with reponse code 200 (OK)
+		builder = Response.ok(fs);
+		builder.tag(tag);
+            }
+            
+        } finally {
+            entityManager.close();
+        }
+        
+        return builder.build();
+    }    
+    
+}
