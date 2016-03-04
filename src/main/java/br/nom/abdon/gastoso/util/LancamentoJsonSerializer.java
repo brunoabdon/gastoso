08b8c73a50ba88452646fba3bf337ad14073e4e4
brunoabdon <bru.n.oa.b.don@gmail.com>
2016-03-04 19:19:17 -0300

    eliminando pacote core

--- src/main/java/br/nom/abdon/gastoso/util/LancamentoJsonSerializer.java
+++ src/main/java/br/nom/abdon/gastoso/util/LancamentoJsonSerializer.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package br.nom.abdon.gastoso.core.util;
+package br.nom.abdon.gastoso.util;
 
 import br.nom.abdon.gastoso.Lancamento;
 import com.fasterxml.jackson.core.JsonGenerator;
