197743a02237f600e05fdfec39bc8384e097d1c2
brunoabdon <bru.n.oa.b.don@gmail.com>
2016-03-21 15:49:00 -0300

    moving system facade to core

---
+++ src/main/java/br/nom/abdon/gastoso/system/GastosoSystem.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 Bruno Abdon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.nom.abdon.gastoso.system;
+
+import br.nom.abdon.gastoso.Conta;
+import br.nom.abdon.gastoso.Fato;
+import br.nom.abdon.gastoso.Lancamento;
+
+import java.util.List;
+
+/**
+ *
+ * @author Bruno Abdon
+ */
+public interface GastosoSystem {
+    
+    public boolean login(final String user, String password) throws GastosoSystemRTException;
+    
+    public List<Fato> getFatos(FiltroFatos filtro) throws GastosoSystemRTException;
+    
+    public Fato getFato(int id) throws NotFoundException, GastosoSystemRTException;
+    
+    public List<Conta> getContas(FiltroContas filtro) throws GastosoSystemRTException;
+    
+    public Conta getConta(int id) throws NotFoundException, GastosoSystemRTException;
+    
+    public List<Lancamento> getLancamentos(FiltroLancamento fitro) throws GastosoSystemRTException;
+            
+    public void update(Fato fato) throws NotFoundException, GastosoSystemRTException;
+    
+    public void update(Conta conta) throws NotFoundException, GastosoSystemRTException;
+    
+    public void update(Lancamento lancamento) throws NotFoundException, GastosoSystemRTException;
+
+    public void delete(Fato fato) throws NotFoundException, GastosoSystemRTException;
+    
+    public void delete(Conta conta)throws NotFoundException, GastosoSystemRTException;
+    
+    public void delete(Lancamento lancamento) throws NotFoundException, GastosoSystemRTException;
+
+    public void create(Fato fato) throws GastosoSystemRTException, GastosoSystemException;
+    
+    public void create(Conta conta) throws GastosoSystemRTException, GastosoSystemException;
+    
+    public void create(Lancamento lancamento) throws GastosoSystemRTException, GastosoSystemException;
+}
